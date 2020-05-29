5318c8d52070ba3498aafa82a7b6774aa75d957f
marcebdev <20135668+marcebdev@users.noreply.github.com>
2020-05-29 14:41:39 -0400

    merge

---
+++ components/HelpSidebar.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <b-sidebar type="is-dark" :fullheight="true" :right="true" :open.sync="open">
+      <div class="column has-text-light has-text-left">
+        <div class="has-text-right" v-on:click="help">
+          <b-icon class="is-medium" icon="help-circle" />
+        </div>
+        <span class="title has-text-light">Help</span><br /><br />
+        <div>{{ helpinfo }}</div>
+      </div>
+    </b-sidebar>
+
+    <div v-on:click="help">
+      <b-icon size="is-small" icon="help-circle" />
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    helpinfo: String
+  },
+  data() {
+    return {
+      open: false
+    };
+  },
+  methods: {
+    help() {
+      this.open = !this.open;
+    }
+  }
+};
+</script>
--- layouts/StoryLayout.vue
+++ layouts/StoryLayout.vue
@@ -7,8 +7,7 @@
     >
       <div class="navbar-brand">
         <n-link class="navbar-item" to="/">
-          <!-- FIXME: use an environment variable -->
-          Plea Simulation Researcher Console
+          {{ scenario_name }}
         </n-link>
 
         <div class="navbar-burger">
@@ -38,8 +37,8 @@
               <span>Log Out</span>
             </button>
           </div>
-          <div class="navbar-item">
-            <b-icon size="is-small" icon="help-circle" />
+          <div class="navbar-item" >
+            <HelpSidebar v-bind:helpinfo="helpinfo" />
           </div>
         </div>
       </div>
@@ -56,14 +55,24 @@
 <script>
 import UploadButton from "~/components/UploadButton";
 import DownloadButton from "~/components/DownloadButton";
+import HelpSidebar from "~/components/HelpSidebar";
 export default {
   components: {
     UploadButton,
-    DownloadButton
+    DownloadButton,
+    HelpSidebar
+  },
+  data() {
+    return {
+      // FIXME: Name should come from manifest or user directory.
+      scenario_name: "Grant Study, Hit and Run",
+      helpinfo: "Variable for information about the storyboard page would appear here with links to the wiki. Click the study name to return to the scenario selection screen."
+    }
   },
   methods:{
     async logout() {
       await this.$auth.logout();
+      this.$router.push("/");
     }
   }
 };
