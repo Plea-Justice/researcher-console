c26b842be76955f7f1da6950603f88330c091b99
marcebdev <20135668+marcebdev@users.noreply.github.com>
2020-08-11 16:03:26 -0400

    Added delete asset modal

--- client/pages/assets.vue
+++ client/pages/assets.vue
@@ -1,14 +1,8 @@
 <template>
-  <ItemLayout
-    contentTitle="Assets"
-    helpTitle="Asset Management"
-    :helpText="assetsHelp.navbar"
-  >
+  <ItemLayout contentTitle="Assets" helpTitle="Asset Management" :helpText="assetsHelp.navbar">
     <template v-slot:toolbar-start>
       <div class="level-item buttons">
-        <ToolBarButton v-model="addMode" @click="toggleAddMode()"
-          >Add</ToolBarButton
-        >
+        <ToolBarButton v-model="addMode" @click="toggleAddMode()">Add</ToolBarButton>
       </div>
     </template>
     <template v-slot:toolbar-end>
@@ -16,9 +10,7 @@
         <!-- FIXME: make this a simplified custom selector -->
         <b-select placeholder="Asset Type" v-model="selectedAssetType">
           <option value="all">All</option>
-          <option v-for="type in validAssetTypes" :key="type" :value="type">{{
-            type | capitalize
-          }}</option>
+          <option v-for="type in validAssetTypes" :key="type" :value="type">{{ type | capitalize }}</option>
         </b-select>
       </b-field>
     </template>
@@ -28,55 +20,33 @@
         <div class="input-wrapper">
           <b-field label="File Upload">
             <b-field>
-              <b-upload
-                v-model="assetForm.file"
-                accept=".js, .jpg, .png"
-                required
-                native
-              >
+              <b-upload v-model="assetForm.file" accept=".js, .jpg, .png" required native>
                 <a class="button is-light">
                   <b-icon size="is-small" icon="cloud-upload-alt" />
                   <span>{{ assetForm.file.name || "Click to upload" }}</span>
                 </a>
               </b-upload>
-              <HelpSidebar
-                :text="assetsHelp.upload"
-                title="Asset Uploads"
-                class="control"
-              />
+              <HelpSidebar :text="assetsHelp.upload" title="Asset Uploads" class="control" />
             </b-field>
           </b-field>
 
           <b-field label="Asset Type">
             <b-field>
-              <b-select
-                placeholder="Select a type"
-                v-model="assetForm.type"
-                expanded
-                required
-              >
+              <b-select placeholder="Select a type" v-model="assetForm.type" expanded required>
                 <option
                   v-for="type in allAssetTypes"
                   :key="type"
                   :value="type"
-                  >{{ type | capitalize }}</option
-                >
+                >{{ type | capitalize }}</option>
               </b-select>
-              <HelpSidebar
-                :text="assetsHelp.type"
-                title="Asset Types"
-                class="control"
-              />
+              <HelpSidebar :text="assetsHelp.type" title="Asset Types" class="control" />
             </b-field>
           </b-field>
         </div>
       </ItemCard>
     </form>
 
-    <p
-      v-if="!addMode && !assetSet.length"
-      class="empty-text has-text-weight-medium is-size-5"
-    >
+    <p v-if="!addMode && !assetSet.length" class="empty-text has-text-weight-medium is-size-5">
       No assets exists!
       <br />Add an asset from the toolbar to get started.
     </p>
@@ -85,7 +55,7 @@
       <ItemCard
         v-for="asset in filteredAssets"
         :key="asset.id"
-        @remove="removeAsset($event)"
+        @remove="confirmDelete($event)"
         :item="asset"
         close
       >
@@ -111,12 +81,13 @@ import ItemLayout from "~/components/layouts/ItemLayout";
 import ToolBarButton from "~/components/ToolBarButton";
 import ItemCard from "~/components/cards/ItemCard";
 import HelpSidebar from "~/components/HelpSidebar";
+import DeleteAsset from "../components/modals/DeleteAsset";
 
 // Content for help fields
 import { assetsHelp } from "~/assets/helpText";
 
 // Last modified time
-import { posixTimeToHoursAgo } from "~/assets/util"
+import { posixTimeToHoursAgo } from "~/assets/util";
 
 export default {
   name: "Scenarios",
@@ -180,15 +151,21 @@ export default {
       addAsset: "assets/addAsset",
       removeAsset: "assets/removeAsset"
     }),
-    confirmDelete(event) {
-      this.$buefy.dialog.confirm({
-        title: "Delete Asset",
-        message:
-          "Deleted assets are not recoverable.<br /><br />Are you sure you want to delete this asset?",
-        confirmText: "Delete",
-        type: "is-danger",
-        hasIcon: true,
-        onConfirm: () => this.removeAsset(event)
+    confirmDelete(id) {
+      let name = "";
+      for (const asset of this.assetSet)
+        if (asset.id === id) {
+          name = asset.name;
+          break;
+        }
+
+      this.$buefy.modal.open({
+        parent: this,
+        component: DeleteAsset,
+        props: { name, onConfirm: () => this.removeAsset(id) },
+        hasModalCard: true,
+        customClass: "dialog",
+        trapFocus: true
       });
     },
     onSubmit() {
