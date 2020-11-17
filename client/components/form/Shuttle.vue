67ed9f10184492d149c7fab61fcca004be2ea3ad
MichaelPascale <32907334+MichaelPascale@users.noreply.github.com>
2020-11-17 12:37:17 -0500

    initial work on transfer box.

---
+++ client/components/form/Shuttle.vue
@@ -0,0 +1,168 @@
+<template>
+  <b-field :label="label">
+      <div class="panel tile is-child is-horizontal">
+
+        <div class="panel-block is-flex is-justify-content-space-between">
+          <p>
+            <b>{{ lhead }}</b>
+          </p>
+          <b-field>
+          <b-button
+            :active="search"
+            @click="search = !search, lcheckedall = checkall(filtered(), true)"
+            icon-right="search"
+            type="is-text"
+            size="is-small"
+          />
+          <b-button
+            @click="lcheckedall = checkall(filtered(), lcheckedall)"
+            :active="lcheckedall"
+            icon-right="check"
+            type="is-text"
+            size="is-small"
+          />
+          </b-field>
+        </div>
+        <div v-if="search" class="panel-block">
+          <b-input
+            size="is-small"
+            placeholder="Type here to filter..."
+            v-model="query"
+          />
+        </div>
+        <template>
+          <label class="panel-block" v-for="item of filtered()" :key="item">
+            <b-checkbox v-model="checked" :native-value="item">
+              {{ item }}
+            </b-checkbox>
+          </label>
+        </template>
+      </div>
+
+      <div
+        class="tile is-child is-horizontal is-1 is-flex is-flex-direction-column is-justify-content-space-evenly is-align-items-center"
+      >
+        <b-button @click="select()" size="is-small" icon-left="angle-right" />
+        <b-button @click="deselect()" size="is-small" icon-left="angle-left" />
+      </div>
+
+      <div class="panel tile is-child is-horizontal">
+        <div class="panel-block is-flex is-justify-content-space-between">
+          <p>
+            <b>{{ rhead }}</b>
+          </p>
+          <!-- FIXME: This button is just to keep sizing of the headers equal. -->
+          <b-button
+            @click="rcheckedall = checkall(selections, rcheckedall)"
+            :active="rcheckedall"
+            class="is-text"
+            icon-right="check"
+            size="is-small"
+          />
+        </div>
+        <template>
+          <label class="panel-block" v-for="item of selections" :key="item">
+            <b-checkbox v-model="checked" :native-value="item">
+              {{ item }}
+            </b-checkbox>
+          </label>
+        </template>
+      </div>
+
+  </b-field>
+</template>
+
+<script>
+import User from "~/mixins/User";
+import FormGroup from "./FormGroup.vue";
+
+export default {
+  components: { FormGroup },
+  mixins: [User],
+  // TODO: Pass back up to parent with event.
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    preselected: {
+      type: Array,
+      default() { return []; },
+      required: false,
+    },
+    keyname: {
+      type: String,
+      default: null,
+      required: false
+    },
+    label: {
+      type: String,
+      default: "Select items from the left column.",
+      required: false,
+    },
+    lhead: {
+      type: String,
+      default: "Options",
+      required: false,
+    },
+    rhead: {
+      type: String,
+      default: "Selections",
+      required: false,
+    },
+  },
+  data() {
+    return {
+      search: false,
+      query: "",
+      lcheckedall: false,
+      rcheckedall: false,
+      selections: this.preselected,
+      checked: [],
+    };
+  },
+  computed: {
+    // filtered() {
+    //   console.log('this;', this)
+    //   return this.options.filter(o => this.selections.indexOf(o) === -1);
+    // }
+  },
+  methods: {
+    filtered() {
+      let list = this.options.filter((o) => this.selections.indexOf(o) === -1);
+
+      if (this.search) {
+        list = list.filter((o) =>
+          o.toLowerCase().startsWith(this.query.toLowerCase())
+        );
+      }
+
+      return list;
+    },
+    select() {
+      for (const item of this.filtered()) {
+        if (this.checked.includes(item)) this.selections.push(item);
+      }
+      this.selections = this.selections.sort();
+      this.lcheckedall = this.rcheckedall = this.checkall([], true);
+      this.$emit('selected', this.selections);
+    },
+    deselect() {
+      this.selections = this.selections.filter(item => !this.checked.includes(item));
+
+      this.selections = this.selections.sort();
+      this.lcheckedall = this.rcheckedall = this.checkall([], true);
+      this.$emit('selected', this.selections);
+    },
+    checkall(array, checkedall) {
+      if (checkedall) this.checked = [];
+      else this.checked = array;
+
+      return !checkedall;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
