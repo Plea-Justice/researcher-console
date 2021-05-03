d605395a1061e9ee455387fff42c1c0229004952
MichaelPascale <32907334+MichaelPascale@users.noreply.github.com>
2021-05-02 20:48:37 -0400

    feat: asset customization interface.

--- client/components/modals/ConditionOptions/AdvancedAssets.vue
+++ client/components/modals/ConditionOptions/AdvancedAssets.vue
@@ -1,48 +1,56 @@
 <template>
-  <form @submit.prevent="onSubmit()" class="modal-card">
+  <form @submit.prevent="onSubmit()" class="modal-card" style="width: 80vw;">
     <header class="modal-card-head">
       <p class="modal-card-title">
         Condition {{ index + 1 }} - Asset Customization
       </p>
     </header>
     <section class="modal-card-body">
-      <i>This is an experimental feature. Check back later.</i>
-      <b-tabs v-model="selectedSlotIndex" expanded multiline>
+      <span><i>This is an experimental feature. Check back later.</i></span>
+      <b-tabs  v-if="selectedSlot && Object.keys(customizableSlots).length" v-model="selectedSlotIndex" expanded multiline>
         <template v-for="(slot, index) in customizableSlots">
           <b-tab-item :key="index" :value="index.toString()">
             <template v-slot:header>
-              <span>Slot #{{ index }}</span>
+              <span>Slot {{ index }} {{ special.slots[index] ? `(${special.slots[index]})` : ''}}</span>
             </template>
 
-            <b-button class="is-small is-pulled-right" type="is-primary">
-              Reset This Slot
-            </b-button>
+            <p class="mb-3" v-if="selectedSlotIndex == 0"><i>By default, customization slot 0
+              is allocated to the participant-avatar. This slot allows you to
+              override the default avatar appearance. Changes made by the
+              participant will take precedent over the changes made here if
+              the avatar customization screen is enabled for this scenario.</i></p>
 
-            <div class="is-size-7">
-              <p class="mb-1">Used by assets:</p>
-              <b-taglist>
+            <span class="is-size-7 is-flex is-flex-direction-row">
+              <p class="my-1 mr-2">Used by assets:</p>
+              <b-taglist class="is-flex-grow-5">
                 <b-tag v-for="asset in slot.assets" :key="asset">{{
                   asset
                 }}</b-tag>
               </b-taglist>
-            </div>
+              <b-tooltip position="is-left" label="Reset all customizations in this slot to the defaults.">
+                <b-button class="is-small" type="is-primary"
+                  @click="resetCurrentSlot">
+                  Reset This Slot
+                </b-button>
+              </b-tooltip>
+            </span>
 
             <div class="flex-fields">
 
-              <div v-if="slot.feature.length > 0" class="numberinputs">
+              <div v-if="slot.feature.length > 0">
                 <h2 class="subtitle">Features</h2>
-                <b-field
-                  v-for="feature in slot.feature"
-                  :key="feature"
-                  :label="feature.name | capitalize"
-                >
-                  <b-numberinput
-                    v-model="selectedSlot.figure"
-                    size="is-small"
-                    min="0"
-                    :max="feature.range - 1"
-                    controls-position="compact"
-                  />
+                <b-field grouped group-multiline class="numberinputs">
+                  <b-field v-for="feature in slot.feature" :key="feature.name"
+                    :label="feature.name | capitalize"
+                    :message="special[feature.name][selectedSlot[feature.name]]">
+                    <b-numberinput
+                      v-model="selectedSlot[feature.name]"
+                      size="is-small"
+                      min="0"
+                      :max="feature.range - 1"
+                      controls-position="compact"
+                    />
+                  </b-field>
                 </b-field>
               </div>
 
@@ -61,38 +69,18 @@
                 </b-field>
               </div>
 
-              <div v-if="slot.toggleable.length > 0">
-                <h2 class="subtitle">Toggleable Layers</h2>
-                <b-field grouped group-multiline>
-                  <b-field
-                    v-for="colorfield in slot.toggleable"
-                    :key="colorfield.color"
-                    :label="colorfield.name"
-                  >
-                    <b-switch></b-switch>
-                  </b-field>
-                </b-field>
-              </div>
+              <!-- TODO: Implement toggleable and numbered layer customizables. -->
 
-              <div v-if="slot.numbered.length > 0" class="numberinputs">
-                <h2 class="subtitle">Selectable Layers</h2>
-                <b-field
-                  v-for="layer in slot.numbered"
-                  :key="layer"
-                  :label="layer.name | capitalize"
-                >
-                  <b-numberinput
-                    size="is-small"
-                    min="0"
-                    :max="layer.range - 1"
-                    controls-position="compact"
-                  />
-                </b-field>
               </div>
             </div>
           </b-tab-item>
         </template>
       </b-tabs>
+      <p v-else> No assets selected. Navigate to the options menu and select assets for this scenario.</p>
+      <p v-if="env.MODE === 'development'">
+          <span>{{customizableSlots}}</span>
+          <pre>{{ slots }}</pre>
+      </p>
     </section>
 
     <footer class="modal-card-foot">
@@ -122,40 +110,22 @@ export default {
     }
   },
   data() {
-    // Initialize slots with the actor defaults.
-    const init = () =>
-      Array.from({ length: 5 }, (x, i) => ({
-        id: i,
-        modified: false,
-        figure: 0,
-        hair: 0,
-        eyes: 0,
-        colors: new Array(6).fill(""),
-        custom: ""
-      }));
-    const slots = init();
-
     return {
       modified: false,
-      slots: slots,
-      selectedSlotIndex: slots.length ? 0 : null,
-      special: ["Avatar", "Judge", "Defense Attorney", "Prosecutor"],
+      slots: [],
+      selectedSlotIndex: 0,
 
-      hair: ["Style 1", "Style 2", "Style 3", "Religious Headwear", "None"],
-      eyes: ["Style 1", "Style 2", "Style 3"],
-      figures: ["Masculine Figure", "Feminine Figure"],
-      colors: ["Eyes", "None", "None", "Hair", "Outfit", "Skin"],
-
-      disableAvatar: true
+      special: {
+        slots: ["Avatar", "Judge", "Defense Attorney", "Prosecutor"],
+        hair: ["Style 1", "Style 2", "Style 3", "Religious Headwear", "None"],
+        eyes: ["Style 1", "Style 2", "Style 3"],
+        figure: ["Masculine Figure", "Feminine Figure"],
+        colors: ["Eyes", "None", "None", "Hair", "Outfit", "Skin"]
+      },
+      
+      disableAvatar: true,
+      env: { MODE: process.env.MODE }
     };
-  },
-  watch: {
-    // slots: {
-    //   handler: function (v) {
-    //     this.modified = true;
-    //   },
-    //   deep: true,
-    // },
   },
   computed: {
     ...mapGetters({
@@ -174,17 +144,21 @@ export default {
     },
     // The reduced set of customization slots for the asset list.
     customizableSlots() {
-      const slots = {};
+      const availableSlots = {};
 
-      const add = (map, obj)=>{
-        if (!map.has(obj.name))
-          map.set(obj.name, obj);
-      }
+      const add = (map, obj) => {
+        if (!map.has(obj.name)) map.set(obj.name, obj);
+      };
 
+      // Determine which customization availableSlots to show.
       const customizables = this.customizableAssets.reduce((acc, asset) => {
-        asset.customizables.forEach(obj => {
-          if (!slots[obj.slot])
-            slots[obj.slot] = {
+
+        // For each customizable used by the asset...
+        asset.customizables.forEach(cstm => {
+
+          // Initialize the slot if it hasn't already been initialized.
+          if (!availableSlots[cstm.slot])
+            availableSlots[cstm.slot] = {
               // List of assets utilizing this slot.
               assets: new Set(),
               // List of customizable colors in this slot.
@@ -196,17 +170,23 @@ export default {
               numbered: new Map()
             };
 
-          slots[obj.slot].assets.add(asset.name);
-          switch (obj.type) {
+          // Add the asset to the list of assets which use the slot.
+          availableSlots[cstm.slot].assets.add(asset.name);
+
+          // Store additional information for certain customizable's.
+          switch (cstm.type) {
             case "color":
-              add(slots[obj.slot].colors, { name: obj.name, color: obj.color });
+              add(availableSlots[cstm.slot].colors, {
+                name: cstm.name,
+                color: cstm.color
+              });
               break;
             case "feature":
             case "toggleable":
             case "numbered":
-              add(slots[obj.slot][obj.type], {
-                name: obj.name,
-                range: obj.range
+              add(availableSlots[cstm.slot][cstm.type], {
+                name: cstm.name,
+                range: cstm.range
               });
               break;
             default:
@@ -216,23 +196,40 @@ export default {
         return acc.concat(asset.customizables);
       }, []);
 
-      Object.values(slots).forEach(
-        slot => {
-          slot.assets = [...slot.assets.values()];
-          slot.colors = [...slot.colors.values()];
-          slot.feature = [...slot.feature.values()];
-          slot.toggleable = [...slot.toggleable.values()];
-          slot.numbered = [...slot.numbered.values()];
-        }
-      );
+      Object.values(availableSlots).forEach(slot => {
+        slot.assets = [...slot.assets.values()];
+        slot.colors = [...slot.colors.values()].sort((x, y) => x.color > y.color);
+        slot.feature = [...slot.feature.values()];
+        slot.toggleable = [...slot.toggleable.values()];
+        slot.numbered = [...slot.numbered.values()];
+      });
 
-      return slots;
+      return availableSlots;
     },
     selectedSlot() {
       return this.slots[this.selectedSlotIndex] || null;
     }
   },
+  mounted: function(){
+    this.slots = Array.from({ length: 6 }, (x, i) => this.init_slot(i));
+  },
   methods: {
+    init_slot(i) {
+      return {
+        id: i,
+        figure: 0,
+        hair: 0,
+        eyes: 0,
+        colors: new Array(6).fill(""),
+        // numbered: this.customizableAssets[i]?.numbered ? this.customizableAssets[i].numbered.reduce(
+        //   (acc, item) => {acc[item.name] = 0, acc}, {}
+        // ) : {},
+        // toggle: []
+      }
+    },
+    resetCurrentSlot() {
+      this.$set(this.slots, this.selectedSlotIndex,this.init_slot(this.selectedSlotIndex));
+    },
     onSubmit() {
       // TODO: Extra processing and validation will have to happen before saving
       // depending on the simulation format.
